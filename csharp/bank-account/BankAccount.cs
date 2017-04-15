9c5d1168c435da0ac58406767429c3e123c47b70
Peter Felice <peter@felicepost.ca>
2017-04-15 00:27:20 -0400

    Completed BankAccount exercise

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class BankAccount
+{
+    bool isOpen = false;
+    float balance = 0;
+    Object refLock = new Object();
+    public void Open()
+    {
+        lock (refLock)
+        {
+            isOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        lock (refLock)
+        {
+            isOpen = false;
+        }
+    }
+
+    public float Balance
+    {
+
+        get
+        {
+            if (isOpen)
+            {
+                return balance;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    public void UpdateBalance(float change)
+    {
+        if (isOpen)
+        {
+            lock (refLock)
+            {
+                balance += change;
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
